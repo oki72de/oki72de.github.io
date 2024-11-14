e9161d0cb8adf27129302f97e5d61757c741a102
Oliver Kindermann <oki@laotse.work>
2024-11-14 14:54:15 +0100

    delete 24 files

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [,{
-    "title": "Hello everyone!",
-    "excerpt":"This is work in progress. I am trying to set up a website with Jekyll and figure out what to do with it. Bear with me.  ","url": "http://localhost:4000/"
-  },{
-    "title": "Test Page",
-    "excerpt":"This is just a test page.   Chapter 1   This makes no sense.   Chapter 2   Local theme or remote theme?   Chapter 3   Still messing around…  ","url": "http://localhost:4000/_pages/test/"
-  }]
